bf02e97b2b2112a6fa853eb822f9076ddcf82c14
(｡◝‿◜｡) <176598310+majinbot@users.noreply.github.com>
2024-11-27 11:37:37 +0200

    idk

--- packages/evm-contracts/hardhat.config.ts
+++ packages/evm-contracts/hardhat.config.ts
@@ -1,6 +1,15 @@
 import type {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
+import "hardhat-deploy"
+import "dotenv/config"
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "https://eth-mainnet.g.alchemy.com/v2/your-api-key"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "api-key"
+
+// Import MNEMONIC or single private key (PK takes precedence)
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 const config: HardhatUserConfig = {
     solidity: {
@@ -12,17 +21,32 @@ const config: HardhatUserConfig = {
             }
         }
     },
+    defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            chainId: 31337
-        }
+            forking: {
+                url: MAINNET_RPC_URL
+            }
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+        },
     },
     paths: {
         sources: "./src",
         tests: "./test",
         cache: "./cache",
         artifacts: "./artifacts"
-    }
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            mainnet: 0, // similarly on mainnet it will take the first account as deployer.
+        },
+        owner: {
+            default: 0,
+        },
+    },
 };
 
 export default config;
